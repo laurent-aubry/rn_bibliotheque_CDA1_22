c4fd9015649977dd12e07682d4ef1c85e81bc192
laurent-aubry <46169816+laurent-aubry@users.noreply.github.com>
2022-02-28 15:35:04 +0100

    TAB Navigator setup

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
 import useFonts from "./hooks/useFonts";
 import AppLoading from "expo-app-loading"; //permet de charger le Font complètement avant le 1er render
 
@@ -11,7 +14,8 @@ import FilmScreen from "./screens/FilmScreen";
 // import NewItemScreen from "./screens/NewItemScreen";
 // import UpdateItemScreen from "./screens/UpdateItemScreen";
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [dataLoaded, setDataLoaded] = useState(false);
@@ -32,16 +36,48 @@ export default function App() {
   }
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName="Home" >
+      {/* <Stack.Navigator initialRouteName="Home" >
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Accueil' }} />
           <Stack.Screen name="Music" component={MusicScreen} options={{ title: 'Musiques' }}
 
           />
           <Stack.Screen name="Film" component={FilmScreen} options={{ title: 'Films' }} />
-          {/* <Stack.Screen name="NewItem" component={NewItemScreen} options={{ title: 'Nouveau' }} />
-          <Stack.Screen name="UpdateItem" component={UpdateItemScreen} options={{ title: 'MAJ' }} /> */}
+          <Stack.Screen name="NewItem" component={NewItemScreen} options={{ title: 'Nouveau' }} />
+          <Stack.Screen name="UpdateItem" component={UpdateItemScreen} options={{ title: 'MAJ' }} />
 
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Musiques") {
+            iconName = focused ? "musical-notes" : "musical-notes-outline";
+          } else if (route.name === "Films") {
+            iconName = focused ? "film" : "film-outline";
+          } else if (route.name === "A propos") {
+            iconName = focused
+              ? "information-circle"
+              : "information-circle-outline";
+          } else if (route.name === "Jeux vidéo") {
+            iconName = focused
+              ? "ios-game-controller"
+              : "ios-game-controller-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "green",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Accueil' }} />
+      <Tab.Screen name="Music" component={MusicScreen} options={{ title: 'Musiques' }} />
+      <Tab.Screen name="Film" component={FilmScreen} />
+      {/* <Tab.Screen name="A propos" component={AProposScreen} /> */}
+    </Tab.Navigator>
     </NavigationContainer>
   );
 }
